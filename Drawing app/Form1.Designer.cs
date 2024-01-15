cb7729b59ad6da3417e09465392c0f35a9d9ff9a
Joshau Momoh <momohjoshua13@gmail.com>
2024-01-15 20:33:21 +0000

    final release

---
+++ Drawing app/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Drawing_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.panel4 = new ogpanel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 630);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(37)))), ((int)(((byte)(39)))));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.button9);
+            this.panel3.Controls.Add(this.button10);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button6);
+            this.panel3.Controls.Add(this.button7);
+            this.panel3.Controls.Add(this.button8);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(206, 527);
+            this.panel3.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.SystemColors.Control;
+            this.button9.Location = new System.Drawing.Point(0, 387);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(204, 43);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.ForeColor = System.Drawing.SystemColors.Control;
+            this.button10.Location = new System.Drawing.Point(0, 344);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(204, 43);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(0, 301);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(204, 43);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.Control;
+            this.button6.Location = new System.Drawing.Point(0, 258);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(204, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.SystemColors.Control;
+            this.button7.Location = new System.Drawing.Point(0, 215);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 43);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.SystemColors.Control;
+            this.button8.Location = new System.Drawing.Point(0, 172);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(204, 43);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(0, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(204, 43);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(0, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(0, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.textbox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // textbox1
+            // 
+            this.textbox1.Location = new System.Drawing.Point(47, 41);
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(100, 22);
+            this.textbox1.TabIndex = 10;
+            this.textbox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(202, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(703, 628);
+            this.panel4.TabIndex = 1;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
+            this.panel4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(905, 628);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Drawing app/Form1.cs
@@ -0,0 +1,478 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_app
+{
+    public class ogpanel : Panel
+    {
+        public ogpanel()
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        private void InitializeComponent2()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+        }
+
+       
+    }
+
+
+
+
+
+
+
+
+    public partial class Form1 : Form
+    {
+        public Point cursor = new Point();
+        public Point cursor1 = new Point();
+
+        public Pen pen = new Pen(Color.White);
+
+        public Graphics g;
+        public Pen rubber = new Pen(Color.Black, 15);
+        public Brush brush = new SolidBrush(Color.White);
+        public Bitmap son;
+        public Image jo;
+    
+        public int lupin = 0;
+        public int u = 0;
+        public bool ji = false;
+
+
+
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = panel4.CreateGraphics();
+            son = new Bitmap(panel4.Width, panel4.Height);
+            g = Graphics.FromImage(son);
+            panel4.BackgroundImage = son;
+            panel4.BackgroundImageLayout = ImageLayout.None;
+            button1.Text = "Colour change";
+            button2.Text = "Rubber";
+            button3.Text = "Size";
+            button4.Text = "Rectangle(filled)";
+            button8.Text = "Circle(filled ellipse)";
+            button7.Text = "Draw Image";
+            button9.Text = "Save Image";
+            button6.Text = "Rectangle";
+            button5.Text = "Circle(ellipse)";
+            button10.Text = "Arc";
+            button3.Click += button3_Click;
+            button4.Click += button4_Click;
+            button8.Click += button8_Click;
+            button7.Click += button7_Click;
+            button6.Click += button6_Click;
+            button5.Click += button5_Click;
+            button10.Click += button10_Click;
+            
+
+
+            panel4.MouseClick += panel4_MouseClick;
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog ui = new ColorDialog();
+            if (ui.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = ui.Color;
+                brush = new SolidBrush(ui.Color);
+
+            }
+
+
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lupin != 1)
+            {
+                u = lupin;
+                lupin = 1;
+
+            }
+            else
+            {
+                lupin = u;
+
+            }
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            cursor1 = e.Location;
+
+        }
+
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+
+        {
+
+            if (lupin == 0 & e.Button == MouseButtons.Left)
+            {
+                cursor = e.Location;
+                g.DrawLine(pen, cursor1, cursor);
+                cursor1 = cursor;
+                panel4.Invalidate();
+            }
+            if (lupin == 1 & e.Button == MouseButtons.Left)
+            {
+                cursor = e.Location;
+                g.DrawLine(rubber, cursor1, cursor);
+                cursor1 = cursor;
+                panel4.Invalidate();
+
+            }
+
+
+        }
+
+        private void panel4_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                if (lupin == 2 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+                    g.FillRectangle(brush, cursor1.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]));
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+
+                if (lupin == 3 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+                    g.FillEllipse(brush, cursor1.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]));
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+                if (lupin == 4 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+                    g.DrawImage(jo, cursor1.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]));
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+                if (lupin == 5 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+                   
+                    
+                        g.DrawRectangle(pen, cursor.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]));
+              
+                    
+                    
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+                if (lupin == 6 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+
+
+                    g.DrawEllipse(pen, cursor.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]));
+
+
+
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+                if (lupin == 7 & e.Button == MouseButtons.Right)
+                {
+                    string u2 = textbox1.Text;
+
+                    string[] re = u2.Split(",".ToCharArray());
+
+                    cursor = e.Location;
+
+
+                    g.DrawArc(pen, cursor.X, cursor.Y, int.Parse(re[0]), int.Parse(re[1]), int.Parse(re[2]), int.Parse(re[3]));
+
+
+
+                    cursor1 = cursor;
+                    panel4.Invalidate();
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Input Measurement Correctly");
+            }
+
+
+
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Input the name ");
+            string p = textbox1.Text;
+            son.Save(p, System.Drawing.Imaging.ImageFormat.Png);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lupin == 0)
+            {
+                try
+                {
+
+
+                    if (ji == false)
+                    {
+                        int u2 = int.Parse(textbox1.Text);
+                        pen.Width = u2;
+                        ji = true;
+
+                    }
+                    else
+                    {
+                        pen.Width = 2;
+                        ji = false;
+                    }
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("type a number ");
+
+                }
+            }
+           
+            if (lupin == 1)
+            {
+                try
+                {
+                    if (ji == false)
+                    {
+                        int u2 = int.Parse(textbox1.Text);
+                        rubber.Width = u2;
+                        ji = true;
+
+                    }
+                    else
+                    {
+                        rubber.Width = 15;
+                        ji = false;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("type a number ");
+
+                }
+            }
+
+
+
+
+
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (lupin != 2)
+            {
+              
+                lupin = 2;
+                MessageBox.Show("You need to intput height,width (in this order and using commas) ");
+
+            }
+            else
+            {
+                lupin = 0;
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (lupin != 3)
+            {
+          
+                lupin = 3;
+               MessageBox.Show("You need to intput height,width (in this order and using commas) ");
+
+            }
+            else
+            {
+                lupin = 0;
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (lupin != 4)
+            {
+                
+                lupin = 4;
+                MessageBox.Show("You need to intput height,width (in this order and using commas) ");
+                OpenFileDialog op = new OpenFileDialog();
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    jo = Image.FromFile(op.FileName);
+                }
+
+            }
+            else
+            {
+                lupin = 0;
+            }
+        }  
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (lupin != 5)
+            {
+                
+                lupin = 5;
+                MessageBox.Show("You need to intput height,width (in this order and using commas) ");
+            }
+            else
+            {
+                lupin = 0;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (lupin != 6)
+            {
+                
+                lupin = 6;
+                MessageBox.Show("You need to intput height,width (in this order and using commas) ");
+
+            }
+            else
+            {
+                lupin = 0;
+            }
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (lupin != 7)
+            {
+              
+                lupin = 7;
+                MessageBox.Show("You need to intput height,width,start angle,sweep angle (in this order and using commas)");
+            }
+            else
+            {
+                lupin = 0;
+            }
+            
+        }
+    }
+}
+    
